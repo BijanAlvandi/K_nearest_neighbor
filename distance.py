9175e99fb9df80910ee537c0a55cbe0254266ce2
Bijan Alvandi <bijan.alvandi@gmail.com>
2025-12-08 23:24:37 +0330

    Add Euclidean distance to distance.py

---
+++ distance.py
@@ -0,0 +1,27 @@
+
+p1 = [1,2,3]
+p2 = [4,5,6]
+
+def euclidean_distance(point1, point2):
+    squared_sum = 0
+    for i in range(len(point1)):
+        difference = point1[i] - point2[i]
+        squared_sum += difference**2
+    return squared_sum**0.5
+
+def manhattan_distance(point1, point2):
+    pass
+
+def chebyshev_distance(point1, point2):
+    pass
+
+def hamming_distance(point1, point2):
+    pass
+
+def cosine_distance(point1, point2):
+    pass
+
+def canberra_distance(point1, point2):
+    pass
+
+euclidean_distance(p1, p2)
